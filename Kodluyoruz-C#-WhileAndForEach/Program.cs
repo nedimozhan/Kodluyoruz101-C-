4fc1be20e2827c5549f829e1ce35d66e7348bc4f
Eyupreisoglu <nedimozhaneyupreisoglu@gmail.com>
2022-03-06 15:52:48 +0300

    While and Foreach

---
+++ Kodluyoruz-C#-WhileAndForEach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+            int counter=1;
+            int sum=0;
+
+            while(counter<=number){
+                sum+=counter;
+                counter++;
+            }   
+
+            Console.WriteLine(sum/number);
+
+            char character='a';
+
+            while(character<'z'){
+                character++;
+            }
+
+
+            string[] cars = {"a","b","c"};
+
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}

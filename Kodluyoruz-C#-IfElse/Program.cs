025bbec0463dc376678fc8527a45be05426e6392
Eyupreisoglu <nedimozhaneyupreisoglu@gmail.com>
2022-03-06 14:46:26 +0300

    If-Else

---
+++ Kodluyoruz-C#-IfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time<=18){
+                Console.WriteLine("Hello");
+            }
+            else{
+                Console.WriteLine("Good Night");
+            }
+
+            string result = time<=18 ? "Hi":"good evening";
+            Console.WriteLine(result);
+
+
+        }
+    }
+}

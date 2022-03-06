5e45ac20dff3a442cf715f9b7d294a9f3c7aeeb6
Eyupreisoglu <nedimozhaneyupreisoglu@gmail.com>
2022-03-06 13:43:25 +0300

    Tyr Catch

---
+++ Kodluyoruz-C#-TryCatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+            
+                Console.Write("Please enter a number : ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The number that you have entered : "+number);
+
+            }
+            catch(Exception ex){
+                Console.WriteLine("Error : "+ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("Complated");
+            }
+
+            try{
+                int a = int.Parse("test");  
+            }
+            catch(FormatException ex){
+                Console.WriteLine("Format error !");
+                Console.WriteLine(ex);
+            }
+
+
+
+        }
+    }
+}

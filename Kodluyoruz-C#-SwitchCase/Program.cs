f3c28647b7c8b66aef936f0fc90d668ed43b709c
Eyupreisoglu <nedimozhaneyupreisoglu@gmail.com>
2022-03-06 15:09:02 +0300

    Switch Case

---
+++ Kodluyoruz-C#-SwitchCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("February");
+                    break;
+                case 3:
+                    Console.WriteLine("January");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                break;
+            }
+
+
+
+        }
+    }
+}
